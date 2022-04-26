ac8766f8bac1e238a2b8125baae09b7717378f3c
Abhiram G P <49094298+armgp@users.noreply.github.com>
2022-04-26 13:01:32 +0530

    Add files via upload

---
+++ src/main/java/com/example/favouns/FavounsApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.favouns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavounsApiApplication {
+
+	public static void main(String[] args) {
+		//start
+		SpringApplication.run(FavounsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/favouns/FavounsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.favouns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavounsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
