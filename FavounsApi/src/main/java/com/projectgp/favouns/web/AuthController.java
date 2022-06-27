ad8a89cf7b13d3f1d323318049633c52734e7fb6
armgp <armgp@users.noreply.github.com>
2022-06-27 17:05:51 +0530

    CORRS solved!

--- FavounsApi/src/main/java/com/projectgp/favouns/web/AuthController.java
+++ FavounsApi/src/main/java/com/projectgp/favouns/web/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.projectgp.favouns.dto.AuthCredentialsRequest;
 import com.projectgp.favouns.model.User;
 import com.projectgp.favouns.utils.JwtUtil;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
